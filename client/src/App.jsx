83733a664d1d2a06ac99b88b07b5ee40b7a50990
priyanshusingh75177-wq <priyanshusingh00031@gmail.com>
2026-03-16 15:35:39 +0530

    Add gitignore and remove node_modules

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/home'
+import Auth from './pages/Auth'
+
+
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path ="/" element={<Home/>}/>
+         <Route path ="/auth" element={<Auth/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
