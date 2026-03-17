e000903519236022d545d73723a27ad9f8ad9223
priyanshusingh75177-wq <priyanshusingh00031@gmail.com>
2026-03-17 11:16:13 +0530

    Added frontend

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,10 +1,22 @@
 
+import React from 'react'
 import { BsRobot } from "react-icons/bs";
 import { IoSparkles } from "react-icons/io5";
 import { motion } from "motion/react";
 import { FcGoogle } from "react-icons/fc";
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../utils/firebase";
 
 function Auth() {
+
+  const handleGoogleAuth = async () => {
+    try {
+      const response = await signInWithPopup(auth, provider)
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
     return (
         <div className='w-full min-h-screen bg-[#f3f3f3] flex items-center justify-center px-6 py-20'>
 
@@ -39,6 +51,7 @@ function Auth() {
                 </p>
 
                 <motion.button
+                onClick={handleGoogleAuth}
                     whileHover={{ opacity: 0.9, scale: 1.03 }}
                     whileTap={{ opacity: 1, scale: 0.98 }}
                     className='w-full flex items-center justify-center gap-3 py-3 bg-black text-white rounded-full shadow-md'
