d020e88da6fbc26f5673ee3058a2a590436d7f98
priyanshusingh75177-wq <priyanshusingh00031@gmail.com>
2026-03-23 17:55:08 +0530

    added the data base

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 import express from "express"
+import dotenv from "dotenv"
+import connectDb from "./config/connectDB.js";
+dotenv.config()
 
 const app = express ()
 
 
-const PORT = 8000
+const PORT = process.env.PORT || 6000 ;
 
 app.get("/",(req,res)=>{
     return res.json({message:"Server started"})
@@ -11,4 +14,5 @@ app.get("/",(req,res)=>{
 
 app.listen(PORT,()=>{
     console.log(`server running on port ${PORT}`)
+    connectDb()
 })
