0aa46231f877ecec487de49979cf3c44032e238e
priyanshusingh75177-wq <priyanshusingh00031@gmail.com>
2026-04-02 13:13:26 +0530

    added server deployment url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import InterviewPage from './pages/InterviewPage'
 import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 
-export const ServerUrl = "http://localhost:8000"
+export const ServerUrl = import.meta.env.VITE_API_URL;
 
 function App() {
  const dispatch = useDispatch()
